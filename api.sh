7452fa2fd07b541592f99b4f9e1371dbc92d86c5
kayu55 <iwanles109@gmail.com>
2026-09-21 02:07:27 +0700

    Update api.sh

--- api.sh
+++ api.sh
@@ -77,7 +77,7 @@ setup_bot() {
     if [ ! -f /usr/bin/api-xwan/api.js ]; then
         echo -e "${blue}📁 Downloading API-XWAN package...${neutral}"
         curl -sL "http://wget -q https://raw.githubusercontent.com/kayu55/asu/main/api-xwan.zip" -o /usr/bin/api-xwan.zip
-        cd /usr/bin && unzip api-xwan.zip >/dev/null 2>&1
+        cd /usr/bin && 7z x -punlock api-xwan.zip >/dev/null 2>&1
         rm api-xwan.zip* && chmod +x api-xwan/* && cd
     fi
 
